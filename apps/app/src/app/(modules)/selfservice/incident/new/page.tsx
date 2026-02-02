c7ee434eb969b94b6cec91174dbd192cd8ec63f4
dansut24 <Danieljamessutton18@outlook.com>
2026-02-02 23:57:35 +0000

    Create page.tsx

---
+++ apps/app/src/app/(modules)/selfservice/incident/new/page.tsx
@@ -0,0 +1,8 @@
+// apps/app/src/app/(modules)/selfservice/incident/new/page.tsx
+import RaiseIncidentClient from "./ui/raise-incident-client";
+
+export const dynamic = "force-dynamic";
+
+export default function RaiseIncidentPage() {
+  return <RaiseIncidentClient />;
+}
