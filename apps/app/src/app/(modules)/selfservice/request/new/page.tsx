02f3cdebed9a67c6c52ad53e2a1ae500e8fc77fb
dansut24 <Danieljamessutton18@outlook.com>
2026-02-03 00:18:31 +0000

    Create page.tsx

---
+++ apps/app/src/app/(modules)/selfservice/request/new/page.tsx
@@ -0,0 +1,8 @@
+// apps/app/src/app/(modules)/selfservice/request/new/page.tsx
+import RaiseRequestClient from "./ui/raise-request-client";
+
+export const dynamic = "force-dynamic";
+
+export default function RaiseRequestPage() {
+  return <RaiseRequestClient />;
+}
