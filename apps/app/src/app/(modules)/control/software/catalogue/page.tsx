8d1741a801f83ac023aef29731989141b7da606f
dansut24 <Danieljamessutton18@outlook.com>
2026-05-21 23:16:43 +0100

    Create page.tsx

---
+++ apps/app/src/app/(modules)/control/software/catalogue/page.tsx
@@ -0,0 +1,7 @@
+import SoftwareCatalogueClient from "./ui/software-catalogue-client";
+
+export const dynamic = "force-dynamic";
+
+export default function SoftwareCataloguePage() {
+  return <SoftwareCatalogueClient />;
+}
