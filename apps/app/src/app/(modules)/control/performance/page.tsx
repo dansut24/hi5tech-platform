364b0aa4fed55882d34498ad2cfcb3281f045d34
dansut24 <Danieljamessutton18@outlook.com>
2026-05-21 14:37:56 +0100

    Create page.tsx

---
+++ apps/app/src/app/(modules)/control/performance/page.tsx
@@ -0,0 +1,7 @@
+import PerformanceDiagnosticsClient from "./ui/performance-diagnostics-client";
+
+export const dynamic = "force-dynamic";
+
+export default function ControlPerformancePage() {
+  return <PerformanceDiagnosticsClient />;
+}
